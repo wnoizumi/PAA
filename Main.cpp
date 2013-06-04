93faf2a9c886c8b4786ed3f2ae800b525a06697c
Willian Nalepa Oizumi <oizumi.willian@gmail.com>
2013-06-04 15:37:22 -0300

    gravando resultados em arquivo

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,8 @@
 
 //#include <stdlib>
 #include <cstdio>
+#include <sstream>
+#include <string>
 #include "Instancia.h"
 #include "ListaEncadeada.h"
 #include "CPUTimer.h"
@@ -41,92 +43,73 @@ void imprime_pares(ListaEncadeada *l) {
 	printf(" }");
 }
 
-int carregaEntrada(int argc, char **argv) {
+int carregaEntrada(char *nomeArquivo) {
 	n = 0;
 	a0b0 = new ParOrdenado();
 	ab = NULL;
 
-	if (argc >= 3) {
-		char *nomeArquivo = (argv[1] + 2);
-		FILE *arquivo = NULL;
-		printf("Lendo instâncias de \"%s\"... ", nomeArquivo);
-		arquivo = fopen(nomeArquivo, "r");
-		if (arquivo == NULL) {
-			printf("Não foi possível abrir \"%s\".\n", nomeArquivo);
-			return EXIT_FAILURE;
-		}
+	FILE *arquivo = NULL;
+	printf("Lendo instâncias de \"%s\"... ", nomeArquivo);
+	arquivo = fopen(nomeArquivo, "r");
+	if (arquivo == NULL) {
+		printf("Não foi possível abrir \"%s\".\n", nomeArquivo);
+		return EXIT_FAILURE;
+	}
 
-		int eof;
-		eof = !fscanf(arquivo, "%d\n", &n);
-		if (eof) {
-			printf("O arquivo \"%s\" está vazio.\n", nomeArquivo);
-			free(a0b0);
-			fclose(arquivo);
-			return EXIT_FAILURE;
-		}
-
-		ab = new ParOrdenado[n];
-
-		eof = !fscanf(arquivo, "%ld\n", &a0b0->a);
-		if (eof) {
-			printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
-			free(a0b0);
-			free(ab);
-			fclose(arquivo);
-			return EXIT_FAILURE;
-		}
-		for (int i = 0; i < n; i++) {
-			eof = !fscanf(arquivo, "%ld\n", &(ab + i)->a);
-			if (eof) {
-				printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
-				free(a0b0);
-				free(ab);
-				fclose(arquivo);
-				return EXIT_FAILURE;
-			}
-		}
-
-		eof = !fscanf(arquivo, "%ld\n", &a0b0->b);
-		if (eof) {
-			printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
-			free(a0b0);
-			free(ab);
-			fclose(arquivo);
-			return EXIT_FAILURE;
-		}
-		for (int i = 0; i < n; i++) {
-			eof = !fscanf(arquivo, "%ld\n", &(ab + i)->b);
-			if (eof) {
-				printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
-				free(a0b0);
-				free(ab);
-				fclose(arquivo);
-				return EXIT_FAILURE;
-			}
-		}
-
+	int eof;
+	eof = !fscanf(arquivo, "%d\n", &n);
+	if (eof) {
+		printf("O arquivo \"%s\" está vazio.\n", nomeArquivo);
+		free(a0b0);
 		fclose(arquivo);
-		printf("%d pares lidos.\n", n);
-	} else {
-		printf("Sintaxe: PPH <numero do algoritmo> <nome do arquivo>\nDemonstração com instância de teste:\n");
-		n = 15;
-		a0b0->a = 334;
-		a0b0->b = 563;
-		ab = new ParOrdenado[n];
-		int a[] = {272, 249, 74, 188, 186, 77, 263, 323, 100, 132, 132, 461, 10, 130, 217};
-		int b[] = {874, 581, 84, 743, 820, 42, 949,  37, 936, 424,  39, 758, 83, 286, 453};
-		for (int i = 0; i < n; i++) {
-			ab[i].a = a[i];
-			ab[i].b = b[i];
+		return EXIT_FAILURE;
+	}
+
+	ab = new ParOrdenado[n];
+
+	eof = !fscanf(arquivo, "%ld\n", &a0b0->a);
+	if (eof) {
+		printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
+		free(a0b0);
+		free(ab);
+		fclose(arquivo);
+		return EXIT_FAILURE;
+	}
+	for (int i = 0; i < n; i++) {
+		eof = !fscanf(arquivo, "%ld\n", &(ab + i)->a);
+		if (eof) {
+			printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
+			free(a0b0);
+			free(ab);
+			fclose(arquivo);
+			return EXIT_FAILURE;
 		}
 	}
+
+	eof = !fscanf(arquivo, "%ld\n", &a0b0->b);
+	if (eof) {
+		printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
+		free(a0b0);
+		free(ab);
+		fclose(arquivo);
+		return EXIT_FAILURE;
+	}
+	for (int i = 0; i < n; i++) {
+		eof = !fscanf(arquivo, "%ld\n", &(ab + i)->b);
+		if (eof) {
+			printf("O arquivo \"%s\" está incompleto.\n", nomeArquivo);
+			free(a0b0);
+			free(ab);
+			fclose(arquivo);
+			return EXIT_FAILURE;
+		}
+	}
+
+	fclose(arquivo);
+	printf("%d pares lidos.\n", n);
 
 	printf("n = %d\n", n);
 	printf("(a0, b0) = (%ld, %ld)\n", a0b0->a, a0b0->b);
-	if (n <= 16) {
-		printf("S = ");
-		imprime_pares_ab(ab, n);
-	}
 
 	return EXIT_SUCCESS;
 }
@@ -150,27 +133,54 @@ double executaAlgoritmo(char algo) {
 	return 0;
 }
 
+void gravarResultado(char algo, char* instancia, double R, CPUTimer totalTime, int k) {
+	FILE *arquivo = NULL;
+	char *nomeArquivo = "resultsPPH";
+	arquivo = fopen(nomeArquivo, "a");
+	if (arquivo == NULL) {
+		printf("Não foi possível abrir \"%s\".\n", nomeArquivo);
+	}
+
+	char buff[200];
+	for (int i = 0; i < 200; ++i) {
+		buff[i] = '\0';
+	}
+	sprintf(buff, "\nalgoritmo %c - instancia: %s T CPU:   %lf   k=%d  ttotal:%lf\n", algo, instancia, totalTime.getCPUTotalSecs() / k , k, totalTime.getCPUTotalSecs());
+
+	fwrite(buff, sizeof(char), sizeof(buff), arquivo);
+	fclose(arquivo);
+}
+
 int main(int argc, char **argv) {
 	printf("\nPPH");
 
-	if (carregaEntrada(argc, argv) == EXIT_SUCCESS) {
-		char algo = argv[1][0];
+	if (argc < 3) {
+		printf("Sintaxe: PPH <numero do algoritmo> <nome do arquivo>\nDemonstração com instância de teste:\n");
+		return EXIT_FAILURE;
+	}
+
+	char algo = argv[1][0];
+	char *nomeArquivo = argv[1] + 3;
+
+	if (carregaEntrada(nomeArquivo) == EXIT_SUCCESS) {
 		printf("\nAlgoritmo %c", algo);
-		CPUTimer totaltime;
-		totaltime.reset();
+		CPUTimer totalTime;
+		totalTime.reset();
 		int k = 0;
 		double R = 0;
 		do {
-			totaltime.start();
+			totalTime.start();
 
 			R = executaAlgoritmo(algo);
 
 			k++;
-			totaltime.stop();
-		} while ( totaltime.getCPUTotalSecs() < 5.0 );
+			totalTime.stop();
+		} while ( totalTime.getCPUTotalSecs() < 5.0 );
+
+		gravarResultado(algo, nomeArquivo, R, totalTime, k);
 
 		printf("\nR = %lf", R);
-		printf("\nT CPU:   %lf   k=%d  ttotal:%lf\n", totaltime.getCPUTotalSecs() / k , k, totaltime.getCPUTotalSecs());
+		printf("\nT CPU:   %lf   k=%d  ttotal:%lf\n", totalTime.getCPUTotalSecs() / k , k, totalTime.getCPUTotalSecs());
 	}
 
 	printf("\n");
